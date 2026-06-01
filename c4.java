cde14f29a1b2111c1d6409a4385f064657c005d0
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-01 16:23:50 +0530

    day 4

---
+++ c4.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class c4 {
+    public static void main(String[] args) {
+        // int count = 0;
+        // while (count<10) {
+        // System.out.println("Hello world");
+        // count ++;
+        // }
+
+        // int i=1;
+        // while (i<=10) {
+        // System.out.println(i);
+        // i++;
+        // }
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter yor last number:");
+        // int n = sc.nextInt();
+        // int i =1;
+        // while (i<=n) {
+        // System.out.println(i);
+        // i++;
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter yor last number:");
+        // int n = sc.nextInt();
+        // int sum=0;
+        // int i=1;
+        // while (i<=n) {
+        // sum =sum+i;
+        // i++;
+        // }
+        // System.out.print("Sum is " +sum);
+        // }
+        // for (int n = 1234; n > 0; n = n / 10) {
+        //     int x = n % 10;
+        //     System.out.print(x);
+        // }
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your number:");
+        // int i = sc.nextInt();
+        // for (int n = i; n > 0; n = n / 10) {
+        //     int x = n % 10;
+        //     System.out.print(x);
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your number:");
+        // int n = sc.nextInt();
+        // int rev =0;z
+        // while (n>0) {
+        //     int ld =n%10;
+        //     rev = (rev*10)+ld;
+        //     n =n/10;
+        // }
+        // System.out.println(rev);
+    }
+}
