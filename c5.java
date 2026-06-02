5bcffc0e08ab30af26a1e6b1f43edb01ff062d61
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-02 13:16:59 +0530

    Day 5

---
+++ c5.java
@@ -0,0 +1,30 @@
+public class c5 {
+    public static void main(String[] args) {
+        // for(int i =1; i<=4; i++){ //row
+        //     for(int j =1; j<=i; j++){    //clumn
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i =1; i<=4; i++){ //row
+        //     for(int j =4; j>=i; j--){    //clumn
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i =1; i<=4; i++){ //row
+        //     for(int j =1; j<=i; j++){    //clumn
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        char ch ='A';
+        for(int i =1; i<=4; i++){ //row
+            for(int j =1; j<=i; j++){    //clumn
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
