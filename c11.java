a4984de782bbc2bee0999311877f8b8e103c7908
Chirag Rewaliya <chirag2006kumar@gmail.com>
2026-06-12 14:26:37 +0530

    Day 12 pattern complete :)

--- c11.java
+++ c11.java
@@ -1,8 +1,128 @@
 public class c11 {
-    public static void hollow_rectangle(int row, int col){
-        for(int i=1; i<=row; i++){
-            for(int j=1; j<=col; j++){
-                if (i==1||i==row||j==1||j==col) {
+    public static void hollow_rectangle(int row, int col) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || i == row || j == 1 || j == col) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void half_paramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotated_paramid(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void invt(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void num(int n) {
+        for (int i = n; i >= 1; i--) {
+            int num = 1;
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num);
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void num_inc(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void tri01(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterfly(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollow_rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++){
+                if (i==1||i==n||j==1||j==n) {
                     System.out.print("*");
                 }
                 else{
@@ -12,10 +132,69 @@ public class c11 {
             System.out.println();
         }
     }
-    
+
+    public static void diamond(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+     public static void num_diamond(int n){
+        int num=1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(num +" ");
+                
+            }
+            System.out.println();
+            num++;
+        }
+    }
+    public static void des_num_diamond(int n){
+        for(int i=1; i<=n; i++){
+             for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // hollow_rectangle(4, 5);
-
-
+        // half_paramid(4);
+        // rotated_paramid(4);
+        // invt(4);
+        // num(5);
+        // num_inc(5);
+        // tri01(5);
+        // butterfly(5);
+        // rhombus(5);
+        // hollow_rhombus(5);
+        // diamond(5);
+        // num_diamond(5);
+        des_num_diamond(5);
     }
 }
